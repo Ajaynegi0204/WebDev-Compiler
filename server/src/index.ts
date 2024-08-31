2ab5c4dbc598dfe9d4ff058a090aee3b53a16f5e
Ajay <negia2582@gmail.com>
2024-09-01 00:05:55 +0530

    fixed cors error

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,29 +1,45 @@
-import express, { Request, Response } from "express"
-import cors from "cors";
-import {config} from "dotenv"
+import express, { Request, Response, NextFunction } from "express";
+import { config } from "dotenv";
 import { dbConnect } from "./lib/dbConnect";
 import { compilerRouter } from "./routes/compilerRouter";
 import { userRouter } from "./routes/userRouter";
 import cookieParser from "cookie-parser";
 
-
 config();
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({credentials: true, origin: ["http://localhost:5173","https://aocompiler.onrender.com"]}));
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://aocompiler.onrender.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Private-Network", "true");
+  res.setHeader("Access-Control-Max-Age", "7200");
 
+  // Handle pre-flight OPTIONS request
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
 
-
+  next();
+});
 
 app.use('/compiler', compilerRouter);
 app.use('/user', userRouter);
 
-
-
 dbConnect();
 app.listen(4000, () => {
-    
-    console.log("Listening on porrrtt 4000");
-})
+  console.log("Listening on port 4000");
+});
